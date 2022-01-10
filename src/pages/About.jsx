2555b5e18767047ce70105b3a5fcace26a9f406e
Santos235 <sr.santos235@gmail.com>
2022-01-10 12:33:24 -0300

    esqueleto about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,6 +3,15 @@ import React from 'react'
 export default function About() {
     return (
         <div className='about'>
+            <div className = "contabout" >
+                <h1>
+                    este é o título
+                </h1>
+
+                <p>
+                    este é o 1° parágrafo
+                </p>
+            </div>
            
         </div>
     )
