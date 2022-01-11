16a69f8eff947d662dd4c177d6bc226ac999aac9
Santos235 <sr.santos235@gmail.com>
2022-01-10 22:22:09 -0300

    colocação de texto

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,14 +5,18 @@ export default function About() {
         <div className='about'>
             <div className = "contabout" >
                 <h1>
-                    este é o título
+                    Sobre Nossa Hamburgueria...
                 </h1>
 
                 
-                <div className = "texto">
+                <div className = "ttexto">
                 <p className = "texto">
-                    este é o 1° parágrafo
+                Somos uma hamburgueria que foi idealizada em 2021 pelo empresário e desenvolvedor Jean Etchichury, que buscava uma nova ideia para o ramo que fosse impactante e tecnológica o suficiente para ganhar o gosto de todos. Com isso, Jean Etchichury foi atrás de desenvolvedores e investidores que compactuassem da mesma ideia. Wellerson Ramos da Silva,Maria Vitória Sousa Duarte Ibiapina, Amadeu Alexandre Filipe e Vitor Antonio dos Santos. Após se inteirar da idéia,   
+Eles adoraram o conceito de hamburgueria tecnológica e queriam experimentá-lo e pôr em prática. Queríamos criar um lugar que fosse de rápido atendimento e extrema qualidade. Todos os sistemas são automatizados e inteligentes. Oferecemos uma variedade de bebidas e acompanhamentos diferentes incentivando nossos clientes a experimentar todo o nosso menu. 
+
                 </p>
+
+                <p className = "texto"> <br /> O texto acima foi criado para emular o “sobre” de uma página de uma hamburgueria real. Textos, site e aplicações foram criados para o trabalho final do curso RESILIA WEBDEV FULLSTAK, no qual, os nomes citados acima são dos criadores da mesma e estão elencados com seus respectivos contatos na página “Contatos”.Com muito carinho e dedicação ao longo de 6 meses, eis nosso projeto que finca como finalizado o curso, mas não os estudos, a dedicação e o amor pela tecnologia. Obrigado a todos os envolvidos e bom apetite!</p>
 
                 </div>
             </div>
