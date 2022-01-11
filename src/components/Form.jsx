0cfb159b2345b2caaf4a034ac32ec864c6592885
jeanetchichury <jean.etchichury@hotmail.com>
2022-01-11 19:58:02 -0300

    Form para Cart pronto+requisição

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,34 +1,31 @@
 import React, { useState } from 'react'
 
-export default function Form() {
-    const [display, setDisplay] = useState("none")
-    const [cep, setCep] = useState({ceps:{}})
-    const [logradouro, setLogradouro] = useState("")
-    const [cidade, setCidade] = useState("")
 
+export default function Form() {
+    const [cepInvalido, setCepInvalido] = useState(false)
+    const [cep, setCep] = useState({ceps:{}})
     
     const reqCeps = async (cepDigitado) => {
         const response = await fetch(`https://viacep.com.br/ws/${cepDigitado}/json`)
         const json = await response.json()
         setCep(json)
         
-        console.log(cep)
-        setLogradouro(cep.logradouro)
-        setCidade(cep.localidade)
-        
-        if (cep.erro){
-            setLogradouro("")
-            setCidade("")
-        }
-        if (cep.localidade != `Niterói`){
-            setDisplay("block")
-        }
-        else{
-            setDisplay('none')
-        }
+        console.log(json)
          
     }
 
+
+    const { logradouro } = cep || ''
+    const { localidade: cidade } = cep || ''
+    const { bairro } = cep || ''
+    let enderecoCompleto = ''
+
+    const pegaEnderecoCompleto = ()=>{
+        if (document.querySelector('#complemento') !== null) {
+            enderecoCompleto = logradouro+" "+document.querySelector('#complemento').value+" "+bairro+" "+cidade
+            console.log(enderecoCompleto)
+        }
+    }
 
     return (
         <form id='form-entrega' 
@@ -37,21 +34,40 @@ export default function Form() {
             <label htmlFor="nome" style={{fontSize: "1.25rem"}}>Nome:</label>
             <input type="text" id='Nome'/>
             <label htmlFor="CEP">CEP</label>
-            <input type="number" id='cep'onBlur={(e) => reqCeps(e.target.value)}/>
-            
-            <p id="nEntrega"style={{
-                color:"red",
-                fontSize: "0.75rem",
-                display: `${display}`
-            }}>Não entregamos neste endereço</p>
-            
+            <input type="number" id='cep'onBlur={(e) => {
+                if (e.target.value.length === 8){
+                    setCepInvalido(false)
+                    reqCeps(e.target.value)
+                }
+                else setCepInvalido(true)
+                }}/>
+            {
+                cepInvalido && (
+                    <p id="nEntrega"style={{
+                        color:"red",
+                        fontSize: "0.75rem",
+                    }}>CEP inválido!</p> 
+                )
+            }
+
+            {
+                (cidade !== "Niterói" && cidade !== undefined )  && (
+                    <p id="nEntrega"style={{
+                        color:"red",
+                        fontSize: "0.75rem",
+                    }}>Não entregamos neste endereço</p> 
+                )
+            }
+
             <label htmlFor="logradouro" style={{fontSize: "1.25rem"}}>logradouro:</label>
             <input type="text" id='logradouro' value={logradouro}/>
             <label htmlFor="complemento" style={{fontSize: "1.25rem"}}>nº/Complemento</label>
-            <input type="text" />
+            <input type="text" id='complemento' onBlur={()=>pegaEnderecoCompleto()}/>
             <label htmlFor="cidade" style={{fontSize: "1.25rem"}}>Cidade</label>
-            <input type="text" value={cidade}/>
-
+            <input type="text" id='cidade' value={cidade}/>
+            <label htmlFor="Bairro" style={{fontSize: "1.25rem"}}>Bairro</label>
+            <input type="text" id='bairro' value={bairro}/>
+            
         </form>
     )
 }
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom'
 import Helmet from '../components/Helmet'
 import CartItem from '../components/CartItem'
 import Button from '../components/Button'
-import Form from '../components/Form'
+// import Form from '../components/Form'
 
 import productData from '../assets/fake-data/products'
 import numberWithCommas from '../utils/numberWithCommas'
@@ -27,12 +27,119 @@ const Cart = () => {
         setTotalProducts(cartItems.reduce((total, item) => total + Number(item.quantity), 0))
     }, [cartItems])
 
+    const [cepInvalido, setCepInvalido] = useState(false)
+    const [cep, setCep] = useState({ceps:{}})
+    
+    const reqCeps = async (cepDigitado) => {
+        const response = await fetch(`https://viacep.com.br/ws/${cepDigitado}/json`)
+        const json = await response.json()
+        setCep(json)
+        
+        console.log(json)
+         
+    }
+
+    const { logradouro } = cep || ''
+    const { localidade: cidade } = cep || ''
+    const { bairro } = cep || ''
+    let enderecoCompleto = ''
+    let pedidos = []
+    let quantidade = []
+
+    const pegaEnderecoCompleto = ()=>{
+        if (document.querySelector('#complemento') !== null) {
+            enderecoCompleto = logradouro+", "+document.querySelector('#complemento').value+", "+bairro+" - "+cidade
+            console.log(enderecoCompleto)
+        }
+    }
+
+    function pegaPedidoQuantidade(){
+        const pedidosSelector = document.querySelectorAll(".cart__item__info__name > a")
+        pedidos = []
+        for (let i = 0; i < pedidosSelector.length; i++) {
+            pedidos.push(pedidosSelector[i].innerText)
+        }
+        const quantidadeSelector = document.querySelectorAll(".product__info__item__quantity__input")
+        quantidade = []
+        for (let i = 0; i < pedidosSelector.length; i++) {
+            quantidade.push(+quantidadeSelector[i].innerText)
+        }
+
+    }
+
+    async function enviaPedido (){        
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({	
+                cliente: document.querySelector('#nome').value,
+                endereco : enderecoCompleto,
+                status_pg : "aguardando pagamento",
+                nome_produto : pedidos,
+                quantidade : quantidade 
+        })
+        };
+        console.log(requestOptions.body)
+        const response = await fetch('https://hamburgueria-resiliente.herokuapp.com/pedido', requestOptions);
+        const data = await response.json();
+        this.setState({ postId: data.id });
+    }
+
+    const pegaEnvia = ()=>{
+        pegaEnderecoCompleto()
+        pegaPedidoQuantidade()
+        enviaPedido()
+        window.location.href = 'pagamento'
+    }
+
     return (
         <Helmet title="CARRINHO">
             <div className="cart">
                 <div lassName="cart__info">
                     <div className="cart__info__txt">
-                        <Form/>
+                        <div>
+                        <form id='form-entrega' 
+                            style={{display : "flex", flexDirection: "column"}}>
+                                <h2>informações Para entrega</h2>
+                                <label htmlFor="nome" style={{fontSize: "1.25rem"}}>Nome:</label>
+                                <input type="text" id='nome'/>
+                                <label htmlFor="CEP">CEP</label>
+                                <input type="number" id='cep'onBlur={(e) => {
+                                    if (e.target.value.length === 8){
+                                        setCepInvalido(false)
+                                        reqCeps(e.target.value)
+                                    }
+                                    else setCepInvalido(true)
+                                    }}/>
+                                {
+                                    cepInvalido && (
+                                        <p id="nEntrega"style={{
+                                            color:"red",
+                                            fontSize: "0.75rem",
+                                        }}>CEP inválido!</p> 
+                                    )
+                                }
+
+                                {
+                                    (cidade !== "Niterói" && cidade !== undefined )  && (
+                                        <p id="nEntrega"style={{
+                                            color:"red",
+                                            fontSize: "0.75rem",
+                                        }}>Não entregamos neste endereço</p> 
+                                    )
+                                }
+
+                                <label htmlFor="logradouro" style={{fontSize: "1.25rem"}}>logradouro:</label>
+                                <input type="text" id='logradouro' value={logradouro}/>
+                                <label htmlFor="complemento" style={{fontSize: "1.25rem"}}>nº/Complemento</label>
+                                <input type="text" id='complemento' onBlur={()=>pegaEnderecoCompleto()}/>
+                                <label htmlFor="cidade" style={{fontSize: "1.25rem"}}>Cidade</label>
+                                <input type="text" id='cidade' value={cidade}/>
+                                <label htmlFor="Bairro" style={{fontSize: "1.25rem"}}>Bairro</label>
+                                <input type="text" id='bairro' value={bairro}/>
+                                
+                            </form>
+                        </div>
                         <p>
                         Você tem {totalProducts} produtos em seu carrinho de compras.
                         </p>
@@ -41,9 +148,9 @@ const Cart = () => {
                         </div>
                     </div>
                     <div className="cart__info__btn">
-                        <Button size="block">
-                                Fazer o Pedido
-                        </Button>
+                            <Button size="block" onClick={()=>pegaEnvia()}>
+                                    Fazer o Pedido
+                            </Button>
                         <Link to="/catalog">
                             <Button size="block">
                             Continue comprando
